81b295a3d7a7ab707f6928ce874b0f4b8eded239
Glitch (test5559) <none>
2020-04-16 04:11:54 +0000

    🐦🔨 Checkpoint ./commands/clear.js:11995214/30 ./commands/cmds.js:11995214/376 ./commands/eval.js:11995214/36 ./commands/info.js:11995214/399

--- commands/clear.js
+++ commands/clear.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premssions to do that!");
   if(!args[0]) return message.channel.send("Please enter a number of messages to clear! `Usage: !clear <amount>`");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`**__Cleared ${args[0]} messages.__**`).then(msg => msg.delete(2000));
+  message.channel.send(`**:white_check_mark:Cleared ${args[0]} messages.**`).then(msg => msg.delete(2000));
 });
 
 
