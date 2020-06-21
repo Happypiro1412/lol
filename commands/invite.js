973ef91208299f407816e5a30a86dec024df82bb
Happypiro1412 <60779157+Happypiro1412@users.noreply.github.com>
2020-06-21 15:57:11 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ exports.run = (bot, message, args) => {
         let embed = new RichEmbed()
     .setAuthor(`*\☆ℋค℘℘ℽ★\*𓅂`, `https://cdn.discordapp.com/attachments/718305766049841210/718307129462620240/JPEG_20200602_131016.jpg`)
     .setThumbnail(bot.user.avatarURL)
-        .addField(`Invite me on your server :kissing_heart:`, `[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=700162701565100063&permissions=8&scope=bot)`, true)
+        .addField(`Invite me on your server :kissing_heart:`, `[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=722790472279523339&permissions=66452800&scope=bot)`, true)
     .setFooter("©Release 2020 | *\☆ℋค℘℘ℽ★\*𓅂 Bot | This bot is still under Development")
     .setTimestamp()
     .setColor(`#11FBFF`)
